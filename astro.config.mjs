c1f28a28258badaafee585ca2bd7ee8977984e3a
Marc Fontova <mfontovat@gmail.com>
2024-01-10 16:51:59 +0100

    dark mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-tutorial-mfontova.netlify.app"
+  site: "https://astro-tutorial-mfontova.netlify.app",
+  integrations: [preact()]
 });
